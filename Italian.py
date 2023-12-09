5c28af896795b48bf8b0113e95e7e666dbf63075
pncl13 <149761784+pncl13@users.noreply.github.com>
2023-12-09 19:39:02 +0700

    Italian.py

--- Italian.py
+++ Italian.py
@@ -1,21 +1,26 @@
 import streamlit as st
-import openai
+from openai import OpenAI
 import json
 import pandas as pd
 
+client = OpenAI()
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
 openai.api_key = user_api_key
 prompt = "Translate the following English text to Italian:\n"
 
 def translate_to_italian(text):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": text},
-        ],
+    response = client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+      ]
     )
+            ],
+        )
     return response['choices'][0]['message']['content']
 
 # Collect interesting words and create a table
