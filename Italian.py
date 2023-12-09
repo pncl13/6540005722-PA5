617e7633dd38ef7792398b0b9d27a477a8b04027
pncl13 <149761784+pncl13@users.noreply.github.com>
2023-12-09 19:16:35 +0700

    Italian.py

--- Italian.py
+++ Italian.py
@@ -11,7 +11,10 @@ prompt = "Translate the following English text to Italian:\n"
 def translate_to_italian(text):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": "system", "content": prompt}, {"role": "user", "content": text}],
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": text},
+        ],
     )
     return response['choices'][0]['message']['content']
 
