252bbfd9492c8461fd5e7061c6ea9912d82425b0
pncl13 <149761784+pncl13@users.noreply.github.com>
2023-12-09 19:42:21 +0700

    Italian.py

--- Italian.py
+++ Italian.py
@@ -14,13 +14,11 @@ def translate_to_italian(text):
       model="gpt-3.5-turbo",
       messages=[
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Good Evening!"},
+        {"role": "assistant", "content": "Buona sera!"},
         {"role": "user", "content": "Where was it played?"}
       ]
     )
-            ],
-        )
     return response['choices'][0]['message']['content']
 
 # Collect interesting words and create a table
